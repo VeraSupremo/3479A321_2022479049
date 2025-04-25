609989928ce87a54ce13426cd3fbf68e10a4c308
VeraSupremo <114789446+VeraSupremo@users.noreply.github.com>
2025-04-25 13:09:47 -0400

    2
    se puso el row, meperdi

--- flutter_application_1/lib/pages/my_home_page.dart
+++ flutter_application_1/lib/pages/my_home_page.dart
@@ -50,19 +50,33 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       persistentFooterButtons: botoncitosPersistentes,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SvgPicture.asset(
-              'assets/icons/atari.svg',
-              semanticsLabel: 'Dart Logo',
-            ),
-            const Text('Presiona el boton artas veces:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        //agregar aqui abajo el card
+        child: Card(
+          color: Colors.teal,
+          elevation: 100 ,
+          margin: EdgeInsets.fromLTRB(4 , 58, 4, 58), //esto es para mover lo sbordes de la tarjeta
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SvgPicture.asset(
+                'assets/icons/atari.svg',
+                semanticsLabel: 'Dart Logo',
+              ),
+              const Text('Presiona el boton artas veces:'),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              //aqui el row pone los botones en horizontal y no en vertical
+              //se puede poner row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround, //el spacearoun va ordenando los botonsitos
+                children:<Widget>[
+                  ElevatedButton(onPressed: _incrementCounter, child:const Icon(Icons.exposure_plus_1))
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
