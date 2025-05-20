38685a99bf047a3775e4e7c00c26001688d67df6
VeraSupremo <martiniverinirx@gmail.com>
2025-05-20 00:37:00 -0400

    333
    intento de solucion al punto 6 creo

--- lib/provider/changeNotifier.dart
+++ lib/provider/changeNotifier.dart
@@ -4,3 +4,15 @@ class AppData extends ChangeNotifier {
   int _counter = 0;
   int get counter => _counter;
 }
+
+void _decrementCounter() {
+  AppData()._counter--;
+}
+
+void _resetCounter() {
+  AppData()._counter = 0;
+}
+
+void _incrementCounter() {
+  AppData()._counter ++;
+}
