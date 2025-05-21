b5002f924eb09741699de4fdc943c45eb0fadd1e
VeraSupremo <martiniverinirx@gmail.com>
2025-05-21 01:39:11 -0400

    Update changeNotifier.dart

--- lib/provider/changeNotifier.dart
+++ lib/provider/changeNotifier.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
+
 class AppData extends ChangeNotifier {
   int _counter = 0;
   int get counter => _counter;
   String nombre = 'Userser';
-  
+
 
   void decrementCounter() {
     AppData()._counter--;
